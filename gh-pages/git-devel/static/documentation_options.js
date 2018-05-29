56afc6aa189274c6343420aa050ce570e4f3a965
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-05-29 23:58:47 +0200

    Generate documentation

---
+++ gh-pages/git-devel/static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.7.0.20180529.3+git6bd35ad',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
---
+++ gh-pages/git-master/static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.0.20-20180529.1+gitb6f0566',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
